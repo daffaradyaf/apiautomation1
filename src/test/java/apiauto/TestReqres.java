5af7eb709914baac0d7166598b19927548d627ac
DaffaPintu <daffa@pintu.co.id>
2024-04-03 18:56:09 +0700

    FIX

--- src/test/java/apiauto/TestReqres.java
+++ src/test/java/apiauto/TestReqres.java
@@ -5,13 +5,20 @@ import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class TestReqres {
 @Test
     public void testGetListUsers () {
-    RestAssured.given().when().get("https://reqres.in/api/users?page=2")
+    given().when()
+            .get("https://reqres.in/api/users?page=2")
             .then().log().all()
-            .assertThat().statusCode(200).assertThat("per_page", Matchers.equalTo(6))
-            .assertThat("page", Matchers.equalTo(2));
+            .assertThat()
+            .statusCode(200)
+            .body("page", equalTo(2))
+            .body("per_page", equalTo(6))
+            .body("total", equalTo(12));
 }
 
 public void testPostCreateUser () {
